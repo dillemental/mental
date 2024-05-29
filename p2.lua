59f45d4a43432e4ebc36002f6318f98ed84ab1b0
dillemental <dillzn@mail.com>
2024-05-29 18:50:25 -0500

    Update and rename p2.lua to test

--- p2.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/aab.lua"))();
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/antist.lua"))();
