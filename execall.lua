9318ed951f7a9fbf2e4a18ca55917a59ff923cc8
dillemental <dillzn@mail.com>
2024-05-29 18:37:10 -0500

    Update execall.lua

--- execall.lua
+++ execall.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/ast.lua"))();
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/atw.lua"))();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/aab.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/atw.lua"))();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/antist.lua"))();
