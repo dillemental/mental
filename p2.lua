c26452f74d8bba6b3c31f88e9566de6ac5c8422e
dillemental <dillzn@mail.com>
2024-05-29 18:44:32 -0500

    Update p2.lua

--- p2.lua
+++ p2.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/atw.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/aab.lua"))();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/antist.lua"))();
