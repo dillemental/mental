5f1dacf22735bbd6fdd72c2952a4f6053bd32651
dillemental <dillzn@mail.com>
2024-05-29 18:28:37 -0500

    Update and rename v1.lua to execall.lua

---
+++ execall.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/v1.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/v1.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/v1.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/v1.lua"))()
