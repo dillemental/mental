ca6c2fceb55a906aa0227d4c7291e748a5748d9d
dillemental <dillzn@mail.com>
2024-05-29 18:41:38 -0500

    Update and rename execall.lua to p1.lua

--- execall.lua
+++
@@ -1,7 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/ast.lua"))();
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/aab.lua"))();
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/atw.lua"))();
-wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/antist.lua"))();
---
+++ p1.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/ast.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/aab.lua"))();
