a021159664e9e89a635e2d8f08c02e38acf794bb
dillemental <dillzn@mail.com>
2024-05-29 20:28:42 -0500

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/ast.lua"))();
+game:GetService("ReplicatedStorage").ReplicatedStorage.Packages.Knit.Services.DungeonService.RF:FindFirstChild(""):InvokeServer();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dillemental/mental/main/atw.lua"))();
